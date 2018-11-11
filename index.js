9547269717d133a691aa5787872b725bdbb1fc1b
abstract-animation-9914 <vm930@hotmail.com>
2018-11-11 21:29:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay (musicians, instruments) {
 
 function johnLennonFacts(facts) {
   let i = 0;
-  while (i < 4) {
+  while (i < parseInt(facts.length)) {
     facts[i] = facts[i] + "!!!";
     i++;
   }
